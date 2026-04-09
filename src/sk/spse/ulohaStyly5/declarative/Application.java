7487ba5c6334565f81776419c97fd9e295487113
Tomáš Ivan <tomas.ivan.it@gmail.com>
2026-04-10 01:13:09 +0200

    Úloha 15.5: Jedálniček - CSS

---
+++ src/sk/spse/ulohaStyly5/declarative/Application.java
@@ -0,0 +1,25 @@
+package sk.spse.ulohaStyly5.declarative;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("primary.fxml"));
+
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Declarative Application 5");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+}
